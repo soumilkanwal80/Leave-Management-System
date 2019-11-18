467b7daa172f33c7bcb7d4a0ce97a19869d433e3
Abhinav Khanna <abhinavkhanna101@gmail.com>
2019-11-18 20:57:52 +0530

    hod_login

--- hod_login_form.py
+++ hod_login_form.py
@@ -4,5 +4,6 @@ from wtforms import StringField,PasswordField,SubmitField
 
 class HODLoginForm(FlaskForm):
 	hod_id = StringField('HOD-ID')
+	dept_name = StringField('Department')
 	password = PasswordField('Password')
 	submit = SubmitField('Log In')
